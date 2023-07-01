2fbca205eee80bbdb75ce82844125504484650fc
Ewan <baileyewan@gmail.com>
2023-07-01 19:24:30 +0100

    turn state

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,10 +3,7 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(
-    ecs: &mut SubWorld,
-    #[resource] map: &Map,
-) {
+pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
     let mut movers = <(&mut Point, &MovingRandomly)>::query();
     movers.iter_mut(ecs).for_each(|(pos, _)| {
         let mut rng = RandomNumberGenerator::new();
